a63cd1912774cbab995568c5aa6e06a779b5e3b7
Mark Johnson <mark.johnson@tauntons.ac.uk>
2012-07-02 11:16:06 +0100

    Bumped version and release number

--- version.php
+++ version.php
@@ -29,9 +29,9 @@
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later (7)
  */
 
-$plugin->version = 2012042700;
+$plugin->version = 2012070200;
 $plugin->cron = 3600;
 $plugin->requires = 2010112400;
 $plugin->component = 'block_accessibility';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '2.0.1 (Build: 2012042700)';
+$plugin->release = '2.0.2 (Build: 2012070200)';
