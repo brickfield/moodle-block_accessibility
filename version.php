940b43e068b1204ab200b490796b2e68b7c8fc5c
Mark Johnson <mark@barrenfrozenwasteland.com>
2012-12-15 14:22:16 +0000

    bumped requires version to 2.2

--- version.php
+++ version.php
@@ -33,7 +33,7 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->version = 2012121500;
 $plugin->cron = 3600;
-$plugin->requires = 2010112400;
+$plugin->requires = 2011120500;
 $plugin->component = 'block_accessibility';
 $plugin->maturity = MATURITY_STABLE;
 $plugin->release = '2.1';
