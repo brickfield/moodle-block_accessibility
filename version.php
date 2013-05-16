0388596dd9e34d0dafdd5a6c3f6992cd32e83461
Mark Johnson <mark.johnson@it.ox.ac.uk>
2013-05-16 12:11:27 +0100

    Updated version number

--- version.php
+++ version.php
@@ -31,9 +31,9 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2013031600;
+$plugin->version = 2013051600;
 $plugin->cron = 3600;
 $plugin->requires = 2011120500;
 $plugin->component = 'block_accessibility';
 $plugin->maturity = MATURITY_STABLE;
-$plugin->release = '2.1.3';
+$plugin->release = '2.1.4';
