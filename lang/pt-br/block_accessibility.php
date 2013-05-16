9a0ab703c9d346219075ac0209ece0cdfa746b05
Mark Johnson <mark.johnson@it.ox.ac.uk>
2013-05-16 12:00:02 +0100

    Added Brazilian portuguese translation, thanks to Fernando Souza

---
+++ lang/pt-br/block_accessibility.php
@@ -0,0 +1,49 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * English Lang Strings                                                (1)
+ *
+ * @author  Fernando Souza                                             (2)
+ * @copyright Fernando Souza 2013	                               (3)
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later (4)
+ */
+defined('MOODLE_INTERNAL') || die();
+
+$string['autolaunch'] = '(sempre?)';
+$string['blockname'] = 'Acesibilidade';
+$string['clearedoldcache'] = 'Arquivos antigos {$a} apagados do cache';
+$string['char'] = 'A';
+$string['dectext'] = 'Diminuir o Tamanho do Texto';
+$string['inctext'] = 'Aumentar o Tamanho do Texto';
+$string['invalidop'] = 'A operação especificada é inválida!';
+$string['jsnocolour'] = 'Erro de mudança no esquema de cores';
+$string['jsnocolourreset'] = 'Erro em redefinir o esquema de cores';
+$string['jsnosave'] = 'Erro ao salvar as configurações';
+$string['jsnosize'] = 'Error ao mudar o tamanho';
+$string['jsnosizereset'] = 'Erro ao redefinir o tamanho do texto';
+$string['launchtoolbar'] = 'Lançar ATbar';
+$string['pluginname'] = 'Acesibilidade';
+$string['pluginnameplural'] = 'Blocos de Acesibilidade';
+$string['resettext'] = 'Redefiniri o Tamanho do Texto (Apaga a Configuração Salva)';
+$string['reset'] = 'Configuração Apagada';
+$string['save'] = 'Salvar Configuração';
+$string['saved'] = 'Configuração Salva';
+$string['col1text'] = 'Esquema de Cores Padrão (Apaga a configuração salva)';
+$string['col2text'] = 'Contraste Baixado 1';
+$string['col3text'] = 'Contraste Baixado 2';
+$string['col4text'] = 'Contraste Alto';
