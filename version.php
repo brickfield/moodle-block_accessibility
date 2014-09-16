bbf27e50329f7664d49db813563b15e76365be2d
Hrvoje Golčić <hrvojegolcic@gmail.com>
2014-09-16 14:27:45 +0200

    IE < 11 adjustments

--- version.php
+++ version.php
@@ -31,7 +31,7 @@
 
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2014072501;
+$plugin->version = 2014072502;
 $plugin->cron = 3600;
 $plugin->requires = 2011120500;
 $plugin->component = 'block_accessibility';
