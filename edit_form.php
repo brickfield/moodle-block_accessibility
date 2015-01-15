150bd8ae7245a6948a30fd7a50292f8db90f3126
Hrvoje Golčić <hrvojegolcic@gmail.com>
2015-01-15 15:11:08 +0100

    Conflict with defined constants resolved

--- edit_form.php
+++ edit_form.php
@@ -2,15 +2,22 @@
 
 // This settings will be available throughout the block.
 // Usage example: $this->config->autosave
-define('CNF_AUTOSAVE', 'config_autosave');
-define('CNF_ATBAR', 'config_showATbar');
-define('CNF_FG', 'config_fg'); // fg1, fg2,...
-define('CNF_BG', 'config_bg'); // bg1, bg2,...
-
-define('RE_COLOUR', '/^#[a-f0-9]{6}$/i');
 
 class block_accessibility_edit_form extends block_edit_form {
+	
+	CONST CNF_AUTOSAVE = 'config_autosave';
+	CONST CNF_ATBAR = 'config_showATbar';
+	CONST CNF_FG = 'config_fg'; // fg1, fg2,...
+	CONST CNF_BG = 'config_bg'; // bg1, bg2,...
+
+	CONST RE_COLOUR = '/^#[a-f0-9]{6}$/i';
+
 	protected function specific_definition($mform) {	
+
+
+
+
+
 
 		// load default colours
 		global $CFG;
@@ -18,27 +25,27 @@ class block_accessibility_edit_form extends block_edit_form {
 		
 		/* not implemented, so far I'm not sure if it's going to be useful
 		// auto-save user settings
-		$mform->addElement('advcheckbox',CNF_AUTOSAVE,
-			get_string (CNF_AUTOSAVE, 'block_accessibility'),
-			get_string (CNF_AUTOSAVE.'_checkbox', 'block_accessibility' ),
+		$mform->addElement('advcheckbox',self::CNF_AUTOSAVE,
+			get_string (self::CNF_AUTOSAVE, 'block_accessibility'),
+			get_string (self::CNF_AUTOSAVE.'_checkbox', 'block_accessibility' ),
 			null,
 			array (0, 1)
 		);
-		$mform->setDefault(CNF_AUTOSAVE, 0);
-		$mform->setType (CNF_AUTOSAVE, PARAM_INT);
-		$mform->addHelpButton(CNF_AUTOSAVE, CNF_AUTOSAVE, 'block_accessibility');
+		$mform->setDefault(self::CNF_AUTOSAVE, 0);
+		$mform->setType (self::CNF_AUTOSAVE, PARAM_INT);
+		$mform->addHelpButton(self::CNF_AUTOSAVE, self::CNF_AUTOSAVE, 'block_accessibility');
 		*/
 
 		// allow ATbar 
-		$mform->addElement('advcheckbox', CNF_ATBAR,
-			get_string (CNF_ATBAR, 'block_accessibility'),
-			get_string (CNF_ATBAR.'_checkbox', 'block_accessibility'),
+		$mform->addElement('advcheckbox', self::CNF_ATBAR,
+			get_string (self::CNF_ATBAR, 'block_accessibility'),
+			get_string (self::CNF_ATBAR.'_checkbox', 'block_accessibility'),
 			null,
 			array (0, 1)
 		);
-		$mform->setDefault(CNF_ATBAR, 1);
-		$mform->setType (CNF_ATBAR, PARAM_INT);
-		$mform->addHelpButton(CNF_ATBAR, CNF_ATBAR, 'block_accessibility');
+		$mform->setDefault(self::CNF_ATBAR, 1);
+		$mform->setType (self::CNF_ATBAR, PARAM_INT);
+		$mform->addHelpButton(self::CNF_ATBAR, self::CNF_ATBAR, 'block_accessibility');
 
 		// An idea: put here default font-size setting?
 
@@ -46,8 +53,8 @@ class block_accessibility_edit_form extends block_edit_form {
 		for($i=2; $i<5; $i++) {  // this is how many declarations we defined in defaults.php
 			// get previously saved configuration
 			$form = $this->block->config; // or cast it to (array) and get properties like with []
-			$fg = str_replace('config_', '', CNF_FG);
-			$bg = str_replace('config_', '', CNF_BG);
+			$fg = str_replace('config_', '', self::CNF_FG);
+			$bg = str_replace('config_', '', self::CNF_BG);
 			$fg_colour = isset($form->{$fg.$i})? $form->{$fg.$i} : $defaults['fg'.$i];
 			$bg_colour = isset($form->{$bg.$i})? $form->{$bg.$i} : $defaults['bg'.$i];
 
@@ -62,20 +69,20 @@ class block_accessibility_edit_form extends block_edit_form {
 			</div>');
 
 			// foreground colour
-			$id = CNF_FG.$i;
-			$mform->addElement('text', $id, get_string(CNF_FG, 'block_accessibility'));
+			$id = self::CNF_FG.$i;
+			$mform->addElement('text', $id, get_string(self::CNF_FG, 'block_accessibility'));
 			$mform->setDefault($id, $defaults['fg'.$i]); 
 			$mform->setType($id, PARAM_TEXT);
-			$mform->addHelpButton($id, CNF_FG, 'block_accessibility');
-			$mform->addRule($id, get_string('color_input_error', 'block_accessibility'), 'regex', RE_COLOUR, 'server', false, false);
+			$mform->addHelpButton($id, self::CNF_FG, 'block_accessibility');
+			$mform->addRule($id, get_string('color_input_error', 'block_accessibility'), 'regex', self::RE_COLOUR, 'server', false, false);
 			
 			// background colour
-			$id = CNF_BG.$i;
-			$mform->addElement('text', $id, get_string(CNF_BG, 'block_accessibility'));
+			$id = self::CNF_BG.$i;
+			$mform->addElement('text', $id, get_string(self::CNF_BG, 'block_accessibility'));
 			$mform->setDefault($id, $defaults['bg'.$i]);
 			$mform->setType($id, PARAM_TEXT);
-			$mform->addHelpButton($id, CNF_BG, 'block_accessibility');
-			$mform->addRule($id, get_string('color_input_error', 'block_accessibility'), 'regex', RE_COLOUR, 'server', false, false);
+			$mform->addHelpButton($id, self::CNF_BG, 'block_accessibility');
+			$mform->addRule($id, get_string('color_input_error', 'block_accessibility'), 'regex', self::RE_COLOUR, 'server', false, false);
 
 		}
 
